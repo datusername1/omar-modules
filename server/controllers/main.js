a3d32ddfe855fd768b8c5dbd05de78dbda598c30
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-10-27 12:23:37 -0700

    setup the inital objec that is going to be added to the database once a get require is sent

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -8,7 +8,25 @@ const controller = {
   },
   post:(req, res) => {
     products.create({
-      
+      name:"N_M_D Originals",
+      price:185.00,
+      icon:"http://placekitten.com/g/200/300",
+      colors:"RED/BLUE/GREEN",
+      size:"9/9.5/10/10.5/11",
+      quantity:100,
+      inventory:1,
+      status:1,
+      favorite:0,
+      shipping:1,
+      category:"Running Shoes",
+      review_count:185,
+      stars:4.5
+    })
+    .then((response) => {
+      res.status(200).send(response)
+    })
+    .catch((err) => {
+      console.log(err)
     })
     console.log("something")
   },
