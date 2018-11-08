7df3dc1a671d183bf5af99eb04fff43671f5c1fc
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-11-07 16:44:09 -0800

    made changes to index in order to get my component rending

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
-// import React from "react";
-// import { render } from "react-dom";
-// import App from "./components/App"
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App"
 
-// render(<App />, document.getElementById('app'))
-
-import Details from "./components/App"
-window.Details = Details
+render(<App />, document.getElementById('details'))
