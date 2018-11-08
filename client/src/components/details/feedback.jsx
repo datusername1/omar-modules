f45c9e545bcef0dc730f505191bf02df5d9c0363
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-11-08 09:02:24 -0800

    I have a fully working module with all the css required using css modules

--- client/src/components/details/feedback.jsx
+++ client/src/components/details/feedback.jsx
@@ -5,18 +5,16 @@ export default function Feedback(props) {
   return(
     <div>
       <div className={styles.detailsFeedback}>
-        <div>
-          <span className={styles.ratings}>
-            <img className={styles.ratingStar} src="star.svg" alt=""/>
-            <img className={styles.ratingStar} src="star.svg" alt=""/>
-            <img className={styles.ratingStar} src="star.svg" alt=""/>
-            <img className={styles.ratingStar} src="star.svg" alt=""/>
-            <img className={styles.ratingStar} src="star.svg" alt=""/>
-          </span>
-          <span className={styles.detailReviews}>
-            <a href=".">Read all {props.product.review_count} reviews</a>
-          </span>
-        </div>
+        <span className={styles.ratings}>
+          <img className={styles.ratingStar} src="https://www.iconsdb.com/icons/preview/black/star-xxl.png" alt=""/>
+          <img className={styles.ratingStar} src="https://www.iconsdb.com/icons/preview/black/star-xxl.png" alt=""/>
+          <img className={styles.ratingStar} src="https://www.iconsdb.com/icons/preview/black/star-xxl.png" alt=""/>
+          <img className={styles.ratingStar} src="https://www.iconsdb.com/icons/preview/black/star-xxl.png" alt=""/>
+          <img className={styles.ratingStar} src="https://www.iconsdb.com/icons/preview/black/star-xxl.png" alt=""/>
+        </span>
+        <span className={styles.detailReviews}>
+          <a href=".">Read all {props.product.review_count} reviews</a>
+        </span>
       </div>
       <div className={styles.productCategory}>  
         <div  className={styles.productCategoryItem}>{props.product.category}</div>
