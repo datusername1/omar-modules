2e4efca0784734dc8359eeab61a75c8091a577d5
omar jandali <omarjandali@omars-MacBook-Pro.local>
2018-11-08 11:56:52 -0800

    added the require for the .env

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const server = require("./bin/www/index.js")
 const express = require("express")
 const normalize = require("normalize-port")
 const databases = require("../database/index.js")
+require('dotenv').config();
 
 const listenPort = 3001
 server.listen(listenPort, () => {console.log(`server is running at http://localhost:${listenPort}`)})
