7758f6083a60a6430d391ca8166d540244563015
haydenbetts <haydenbetts@gmail.com>
2018-11-23 20:42:41 -0800

    [progress] refactor css classes so all links inherit on-hover propety from the same place

--- client/src/components/breadcrums.jsx
+++ client/src/components/breadcrums.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import styles from "../css/breadcrums.css"
+import styles from "../css/breadcrums.css";
+import appStyles from "../css/app-style.css";
 
 export default function Breadcrums(props) {
   return (
@@ -7,31 +8,31 @@ export default function Breadcrums(props) {
       <div className={styles.breadcrumsSection}>
         <ol className={styles.breadCrumsList}>
           <li className={styles.breadcrumListItemSection}>
-            <button className={styles.breadcrumListItemButton}>
+            <button className={[styles.breadcrumListItemButton, appStyles.link].join(' ')}>
               <img className={styles.breadCrumListItemImage} src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-arrow-back-512.png" alt="" />
-              <span className={styles.breadCrumBack}>Back</span>
+              <a className={[styles.breadCrumMainItem, appStyles.link].join(' ')}>Back</a>
             </button>
           </li>
           <li className={styles.breadcrumListItemSection}>
-            <a className={styles.breadCrumMainItem} href="">
+            <a className={[styles.breadCrumMainItem, appStyles.link].join(' ')} href="">
               <span>Home</span>
             </a>
           </li>
           <span className={styles.breadCrumMainItemSeperator}>/</span>
           <li className={styles.breadcrumListItemSection}>
-            <a className={styles.breadCrumMainItem} href="">
+            <a className={[styles.breadCrumMainItem, appStyles.link].join(' ')} href="">
               <span>{props.product.gender}'s</span>
             </a>
           </li>
           <span className={styles.breadCrumMainItemSeperator}>/</span>
           <li className={styles.breadcrumListItemSection}>
-            <a className={styles.breadCrumMainItem} href="">
+            <a className={[styles.breadCrumMainItem, appStyles.link].join(' ')} href="">
               <span>{props.product.category}s</span>
             </a>
           </li>
           <span className={styles.breadCrumMainItemSeperator}>/</span>
           <li className={styles.breadcrumListItemSection}>
-            <a className={styles.breadCrumMainItem} href="">
+            <a className={[styles.breadCrumMainItem, appStyles.link].join(' ')} href="">
               <span>{props.product.name}</span>
             </a>
           </li>
