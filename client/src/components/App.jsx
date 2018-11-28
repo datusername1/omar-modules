33c91e987133ffd4504273ce3d6eb701d8d68c6d
tim <timothy.tran835@gmail.com>
2018-11-28 12:37:45 -0800

    changed correct Url pathing for data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,50 +1,51 @@
-import React, { Component } from "react";
-import Details from "./details/details";
-import Carousel from "./carousel/carousel";
-import Complete from "./details/completeLook";
-import Breadcrum from "./breadcrums"
-import axios from "axios";
+import React, { Component } from 'react';
+import Details from './details/details';
+import Carousel from './carousel/carousel';
+import Complete from './details/completeLook';
+import Breadcrum from './breadcrums';
+import axios from 'axios';
 import styles from '../css/app-style.css';
 
 export default class App extends Component {
   constructor(props) {
     super(props),
-      this.state = {
-        product: "",
-        featured: "",
+      (this.state = {
+        product: '',
+        featured: '',
         options: [],
         images: [],
         magnify: false,
-      }
-    axios.defaults.baseURL = "http://" + process.env.HOSTNAME + ":" + process.env.PORT;
+      });
+    // axios.defaults.baseURL = "http://" + process.env.HOSTNAME + ":" + process.env.PORT;
+    axios.defaults.baseURL =
+      'http://ec2-35-162-25-38.us-west-2.compute.amazonaws.com';
   }
 
-
   componentDidMount() {
-    console.log("http://" + process.env.HOSTNAME + ":" + process.env.PORT)
-    console.log("supssssss")
-    this.fetchProduct()
+    console.log('http://' + process.env.HOSTNAME + ':' + process.env.PORT);
+    console.log('supssssss');
+    this.fetchProduct();
   }
 
   generateRandomNumber(value) {
-    return Math.floor(Math.random() * value)
+    return Math.floor(Math.random() * value);
   }
 
   fetchProduct() {
     axios
-      .get("/api/product")
-      .then((response) => {
-        console.log(response)
+      .get('/api/product')
+      .then(response => {
+        console.log(response);
         this.setState({
           product: response.data,
           featured: response.data.featured,
-          options: response.data.options.split(","),
-          images: response.data.images.split(",")
-        })
+          options: response.data.options.split(','),
+          images: response.data.images.split(','),
+        });
       })
-      .catch((err) => {
-        console.log(err)
-      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
@@ -52,21 +53,26 @@ export default class App extends Component {
       <div className={styles.moduleMargin}>
         <Breadcrum product={this.state.product} />
         <div className={styles.productDetails}>
-          <div className={styles.banner}></div>
+          <div className={styles.banner} />
           <div className={styles.detailsContent}>
             <div className={styles.productCarousel}>
-              <Carousel featured={this.state.featured} images={this.state.images} magnification={this.magnification} />
+              <Carousel
+                featured={this.state.featured}
+                images={this.state.images}
+                magnification={this.magnification}
+              />
             </div>
-            <div className={styles.status}>
-              NEW
-            </div>
+            <div className={styles.status}>NEW</div>
             <div className={styles.details}>
-              <Details product={this.state.product} options={this.state.options} />
+              <Details
+                product={this.state.product}
+                options={this.state.options}
+              />
             </div>
             <Complete product={this.state.product} />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
