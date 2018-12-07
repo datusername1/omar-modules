1880d6f51eeaf8123094157bdab4bb87c79a0d65
tim <timothy.tran835@gmail.com>
2018-12-06 16:39:36 -0800

    got popup background to work, will work on css to render all info

--- client/src/components/details/purchase/popup/Popup.jsx
+++ client/src/components/details/purchase/popup/Popup.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from '../../../../css/purchase/popup/Popup.css';
+import { fakeSizeData} from '../../../../../../sample_data_generation_refactor/fakeSizes/fakeSizes';
 
 export default function Popup(props) {
+
+  const 
   return (
-    <div>
+    <div className={styles.popupInner}>
       <div>Hello</div>
+      <button className={styles.button} onClick={props.renderPopup}>
+        X
+      </button>
     </div>
   );
 }
--- client/src/components/details/purchase/sizeChart.jsx
+++ client/src/components/details/purchase/sizeChart.jsx
@@ -6,8 +6,19 @@ import appStyles from '../../../css/app-style.css';
 export default class SizeChart extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      popupState: false,
+    };
+
+    this.renderPopup = this.renderPopup.bind(this);
   }
+
+  renderPopup() {
+    this.setState({
+      popupState: !this.state.popupState,
+    });
+  }
+
   render() {
     return (
       <div className={styles.sizeChartItem}>
@@ -16,9 +27,17 @@ export default class SizeChart extends Component {
           src="https://png.icons8.com/metro/1600/ruler.png"
           alt=""
         />
-        <a onClick href="" class={appStyles.link}>
+        <div onClick={this.renderPopup} href="" class={appStyles.link}>
           Size Chart
-        </a>
+        </div>
+        {this.state.popupState ? (
+          <div className={styles.popup}>
+            <Popup
+              renderPopup={this.renderPopup}
+              className={styles.popupInner}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
